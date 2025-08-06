46fb2e39b2152458544a52204c6970d81d23f48f
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-06 04:42:05 -0300

    Criando a página de erro

---
+++ src/pages/Erro/index.js
@@ -0,0 +1,10 @@
+function Erro() {
+    return(
+        <div>
+            <h1>404</h1>
+            <h2>Ops! Parece que essa página não existe!</h2>
+        </div>
+    )
+}
+
+export default Erro;
