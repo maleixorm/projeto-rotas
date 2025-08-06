702f48ed65c8a32e0a5f2b4c71639fea856f2155
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-06 04:22:21 -0300

    Add header no RoutesApp

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Contato from "./pages/Contato";
+import Header from "./components/Header";
 
 function RoutesApp(params) {
     return(
         <BrowserRouter>
+            <Header/>
             <Routes>
                 <Route path="/" element={ <Home/> } />
                 <Route path="/sobre" element={ <Sobre/> } />
